7abe65168df1b74833fca771f9a02d3f3e7a81c9
Gagan Khanna <gagan.khanna@avanade.com>
2023-10-02 14:29:22 -0600

    Display a list of heroes - Addressess #2

---
+++ tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import {Hero} from './hero';
+
+// Define a HEROES constant as an array of ten 2009 Indian film heroes.  
+// Export the constant so it can be imported elsewhere, such as in the HeroesComponent.
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Shahrukh Khan' },
+    { id: 12, name: 'Salman Khan' },
+    { id: 13, name: 'Aamir Khan' },
+    { id: 14, name: 'Akshay Kumar' },
+    { id: 15, name: 'Ajay Devgan' },
+    { id: 16, name: 'Hrithik Roshan' },
+    { id: 17, name: 'Ranbir Kapoor' },
+    { id: 18, name: 'Ranveer Singh' },
+    { id: 19, name: 'Varun Dhawan' },
+    { id: 20, name: 'Tiger Shroff' }
+];
