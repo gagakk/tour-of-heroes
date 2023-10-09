920e967c84be7e85667f84dfa343f0e6a93fe313
Gagan Khanna <gagan.khanna@avanade.com>
2023-10-09 15:22:12 -0600

    POST request and update hero - Addressess #20

--- tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -18,11 +18,20 @@ export class HeroDetailComponent {
    this.getHero();
   }
 
+  // Add a getHero() method to fetch the hero from the service.
   getHero(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.heroService.getHero(id).subscribe(x => this.hero = x);
   }
 
+  // Save the hero after editing and go back to the previous page.
+  save(): void {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero).subscribe(() => {this.goBack()});
+    }
+  }
+
+  // Go back to the previous page.
   goBack(): void {
     this.location.back();
   }
--- tour-of-heroes/src/app/hero.service.ts
+++ tour-of-heroes/src/app/hero.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { Observable } from 'rxjs';
 import { MessageService } from './message.service';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -23,5 +23,10 @@ export class HeroService {
     const hero = this.httpClient.get<Hero>(`http://127.0.0.1:5000/detail/${id}`);
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return hero;
+  }
+
+  // Add a updateHero method to update the hero on the server.
+  updateHero(hero: Hero): Observable<Hero> {
+    return this.httpClient.post<Hero>(`http://127.0.0.1:5000/update`, hero);
   }
 }
